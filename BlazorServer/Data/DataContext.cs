66661ddf0de25e20444f64ff1df304705dcf0db5
crisvargas03 <civargas34@gmail.com>
2022-07-25 22:20:34 -0400

    Add project files.

---
+++ BlazorServer/Data/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServer.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>().HasData(
+                new Game
+                {
+                    Id = 1,
+                    Name = "God Of War",
+                    Developer = "Santa Monica Studios",
+                    Release = new DateTime(2018, 11, 15)
+                },
+                new Game
+                {
+                    Id = 2,
+                    Name = "NBA 2K25",
+                    Developer = "2k",
+                    Release = new DateTime(2024, 11, 15)
+                },
+                new Game
+                {
+                    Id = 3,
+                    Name = "Marvel's Spiderman",
+                    Developer = "La gente dura",
+                    Release = new DateTime(2018, 11, 15)
+                }
+            );
+        }
+
+        public DbSet<Game> Games => Set<Game>();
+    }
+}
---
+++ BlazorServer/Program.cs
@@ -0,0 +1,34 @@
+using BlazorServer.Data;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("videogameDB"));
+builder.Services.AddScoped<IGameService, GameService>();
+builder.Services.AddSweetAlert2();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
